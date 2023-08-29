c8f608e7657265c867485226dceee13847d30f1a
Anurag Sawant <annuragsawant@duck.com>
2023-08-29 21:19:30 +0530

    added cors header

--- server/index.js
+++ server/index.js
@@ -36,6 +36,13 @@ const corsOptions = {
 console.log(process.env.FRONTEND_ADDRESS);
 logger.info(process.env.FRONTEND_ADDRESS);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(express.json());
