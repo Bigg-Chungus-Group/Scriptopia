31c56b8177667dfd74f2b44f37348141d4cdeefd
Anurag Sawant <anuragsawant@duck.com>
2023-09-17 14:01:40 +0530

    Update Certificates.jsx

--- client/src/pages/Student/Certificates/Certificates.jsx
+++ client/src/pages/Student/Certificates/Certificates.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import Navbar from "../../../components/student/Navbar/";
+import Navbar from "../../../components/Student/Navbar/";
 import {
   Box,
   Table,
