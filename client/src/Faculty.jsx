973217fbc41cdf1a449a30d7a6b4a8fadb4a4dfd
Anurag Sawant <annuragsawant@duck.com>
2023-10-08 16:20:15 +0530

    Updated Wrong Path for Faculty

--- client/src/Faculty.jsx
+++ client/src/Faculty.jsx
@@ -1,6 +1,6 @@
 import { Route } from "react-router-dom";
 import React from "react";
-import FacultyHome from "./pages/faculty/Home/Home.jsx";
+import FacultyHome from "./pages/Faculty/Home/Home.jsx";
 import Certificates from "./pages/Faculty/Certificates/Certificates.jsx";
 import Enrollments from "./pages/Faculty/Enrollments/Enrollments.jsx";
 import FacultySettings from "./pages/Faculty/Settings/Settings.jsx";
